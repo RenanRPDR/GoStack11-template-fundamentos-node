6a1b623008a5c778f893e3bca81de6591e82872b
RenanRPDR <55991676+RenanRPDR@users.noreply.github.com>
2020-05-04 19:47:45 -0300

    Add files via upload
    Rota post implementada.

---
+++ TransactionsRepository.ts
@@ -0,0 +1,39 @@
+import Transaction from '../models/Transaction';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface Balance {
+  income: number;
+  outcome: number;
+  total: number;
+}
+
+class TransactionsRepository {
+  private transactions: Transaction[];
+
+  constructor() {
+    this.transactions = [];
+  }
+
+  public all(): Transaction[] {
+    // TODO
+  }
+
+  public getBalance(): Balance {
+    // TODO
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+}
+
+export default TransactionsRepository;
